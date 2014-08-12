95136212f8ec6d4ae374b43a12efc10451113372
Jeffrey Clark <ayukawa@gmail.com>
2014-08-12 12:00:05 -0400

    Code and comment cleanup.

--- textual-ircop/textual-ircop.swift
+++ textual-ircop/textual-ircop.swift
@@ -43,8 +43,20 @@ func =~ (input: String, pattern: String) -> [String]? {
 
 class TPI_IRCopPlugin: NSObject, THOPluginProtocol
 {
+    /*
+        TODO: I dislike how this file/path is hardcoded and named.  I'd like to get more elaborate
+        in the future.  Possibly support multiple filter files, configured through the GUI.  Maybe.
+    */
+    let dataFile = NSHomeDirectory().stringByAppendingPathComponent("Documents/dalnet-filters.xml");
     
+    var filterArray: [OperFilter] = []
     @IBOutlet var ourView:NSView! = NSView()
+    
+    
+    /*
+        This SHOULD add the config window to the Textual preferences pane.
+        ..it doesn't.
+    */
     func pluginPreferencesPaneView() -> (NSView)
     {
         if (self.ourView == nil) {
@@ -55,71 +67,37 @@ class TPI_IRCopPlugin: NSObject, THOPluginProtocol
         return self.ourView;
     }
     
+    
+    /*
+        Add the plugin's preferences sheet to Textual's config pane list.
+    */
     func pluginPreferencesPaneMenuItemName() ->(NSString)
     {
         return "IRCop Extensions"
     }
     
-    
-    
-    /*
-        TODO: I dislike how this file/path is hardcoded and named.  I'd like to get more elaborate
-            in the future.  Possibly support multiple filter files, configured through the GUI.  Maybe.
+   
+    /* 
+        Open and parse the XML configuration file, placing the results into filterArray
     */
-
-    let dataFile = NSHomeDirectory().stringByAppendingPathComponent("Documents/dalnet-filters.xml");
-    //var filterExpressions = [String:String]()
-    
-    var filterArray: [OperFilter] = []
-    
-    
-    
     func loadFilterSet(client :IRCClient) {
-        //let fileString = String.stringWithContentsOfFile(self.dataFile, encoding: NSUTF8StringEncoding, error: nil) as String!
-        
         let filterParser = SwiftXMLParser(fromFileAtPath: self.dataFile)
-        //let fileString = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: nil) as String!
-        //println(fileString)
         
         filterParser.start()
         filterArray = filterParser.getParsedItems()
         
         self.writeToWindow(client, text: "Loaded \(filterArray.count) filters in \(filterParser.getLastDuration()) seconds.")
-        //myParser[0]
-//        myParser.displayItems()
-        
-        
-        
-        
-        
-        /*
-        if (fileString != nil) {
-            let lines = fileString.componentsSeparatedByString("\n") as [String];
-            
-            self.filterExpressions =  [String: String]()
-            
-            for l in lines {
-                if (!l.hasPrefix("#")) {
-                    var tmpString = l.componentsSeparatedByString("🐶") as [String]; //Yes, we're using a dog to tokenize.
-                    if (tmpString.count == 2) {
-                        self.filterExpressions[tmpString[0]] = tmpString[1];
-                    }
-                }
-            }
-            self.writeToWindow(client, text: "Reloading \(self.dataFile)")
-        } else {
-            self.writeToWindow(client, text: "Unable to open \(self.dataFile)")
-        }*/
+
     }
     
-    func showFilters(client :IRCClient) {
+
+    /*
+        Displays a list of loaded filters into the @Operator window.  Mostly for debugging.
+    */
+    func showFilters(client: IRCClient) {
         for of in filterArray {
             writeToWindow(client,text:"Expression: \(of.expression)")
         }
-        /*
-        for (regex,filter) in self.filterExpressions {
-            self.writeToWindow(client,text:"Regex: \(regex) - Filter: \(filter)");
-        }*/
     }
     
    /*
@@ -139,7 +117,7 @@ class TPI_IRCopPlugin: NSObject, THOPluginProtocol
     return @"My Test Plugin";
     }*/
     
-    
+    /*
     
     func pluginSupportsServerInputCommands() -> (NSArray)
     {
@@ -150,14 +128,23 @@ class TPI_IRCopPlugin: NSObject, THOPluginProtocol
         different actions for each value. */
         
         return ["notice"]
-    }
+    }*/
     
+    
+    /*
+        Creates the @Operator window for us to send messages to.
+    */
     func buildOperatorWindow(client :IRCClient) {
         //client.findChannelOrCreate("@Operator",isPrivateMessage:true)
         
         worldController().createPrivateMessage("@Operator", client: client)
     }
     
+    
+    /*
+        Intercepts all data coming from server.  NOTICE messages will be set to the plugin 
+        handler, everything else will just be passed along.
+    */
     func interceptServerInput(input: IRCMessage!, `for` client: IRCClient!) -> IRCMessage! {
         let commandValue = (input.command as String);
         //let message = input.paramAt(1);
@@ -171,81 +158,80 @@ class TPI_IRCopPlugin: NSObject, THOPluginProtocol
     }
     
     
-    //func handleIncomingNoticeCommand(client :IRCClient, sender: NSDictionary, message: NSDictionary)
+    /*
+        Run server notices against filter array and reformat matches.  Returns nil if there's a
+        handled match, and returns the original IRCMessage if no filter is found.
+    */
     func handleIncomingNoticeCommand(client :IRCClient, input: IRCMessage!) -> IRCMessage!
     {
         if (filterArray.count < 1) { self.loadFilterSet(client) }
         var mParamString = "";
         let messageRecieved = input.paramAt(1)
         
-        if (messageRecieved.hasPrefix("***")) {
-        //if (messageRecieved.substring(0,length: 3) == "***") {
-            //We should be able to safely assume this is a server notice
-            
+        if (messageRecieved.hasPrefix("***")) {   //We should be able to safely assume this is a server notice
             for of in filterArray {
-            
-            //for (expression, result) in self.filterExpressions {
                 if of.enabled.lowercaseString=="yes" {
-//                    writeToWindow(client,text:"\(messageRecieved) vs. \(of.expression)")
-                if var matches = messageRecieved =~ of.expression {
-                    if of.enabled.lowercaseString == "halt" { return nil }
-                    
-                    //if (result == "@HALT@") { return nil; }
-                    
-                    var formattedString = of.format;//result;
-                    for i in 0..<matches.count {
+                    if var matches = messageRecieved =~ of.expression {
+                        if of.enabled.lowercaseString == "halt" { return nil }
                         
-                        formattedString = formattedString.stringByReplacingOccurrencesOfString("@M_\(i)@", withString: matches[i], options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+                        var formattedString = of.format;//result;
+                        for i in 0..<matches.count {
+                            formattedString = formattedString.stringByReplacingOccurrencesOfString("@M_\(i)@", withString: matches[i], options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+                            
+                        }
+                        //Apparently we need to process colors by hand.
+                        let uCodes: [String: String] = ["@B@": "\u{0002}", "@I@": "\u{001D}", "@U@": "\u{001F}", "@C@": "\u{0003}"]
                         
+                        for (at,uc) in uCodes {
+                            formattedString = formattedString.stringByReplacingOccurrencesOfString(at, withString:uc, options: NSStringCompareOptions.LiteralSearch, range:nil)
+                        }
+                        
+                        self.writeToWindow(client, text: formattedString)
+                        
+                        return nil; //We've handled the notice, Textual doesn't need to process it further.
                     }
-                    
-                    //Apparently we need to process colors by hand.
-                    let uCodes: [String: String] = /*["@BOLD@": "\u{0002}", "@ITALIC@": "\u{001D}", "@UNDERLINE@": "\u{001F}", "@COLOR@": "\u{0003}",*/ ["@B@": "\u{0002}", "@I@": "\u{001D}", "@U@": "\u{001F}", "@C@": "\u{0003}"]
-                    
-                    for (at,uc) in uCodes {
-                        formattedString = formattedString.stringByReplacingOccurrencesOfString(at, withString:uc, options: NSStringCompareOptions.LiteralSearch, range:nil)
-                    }
-                    
-                    self.writeToWindow(client, text: formattedString)
-                    
-                    return nil; //We've handled the notice, Textual doesn't need to process it further.
-                }
                 }
             }
-            
         }
         return input; //Nothing matched, let Textual handle it.
     }
     
+    
+    /*
+        Writes text to the @Operator window, creating the window if it does not exist.
+    */
     func writeToWindow(client: IRCClient, text: String)
     {
-        //var chan: IRCChannel = client.findChannel("@Operator")
-        
-        //Write to the server window.  This should eventually write to the @Operator window.
         if (client.findChannel("@Operator") == nil) { self.buildOperatorWindow(client) }
         
         client.iomt().print(client.findChannel("@Operator"), type:TVCLogLineDebugType, nickname:nil, messageBody: text, command: TVCLogLineDefaultRawCommandValue)
         client.findChannel("@Operator").treeUnreadCount++
     }
     
+    
+    /*
+        Returns an array of user commands supported, expecting all caps.  
+        These commands will show in the plugin dialog.
+    */
     func pluginSupportsUserInputCommands() -> (NSArray)
     {
-        //Simply returns a list of the user commands supported.  They will show in the plugin dialog.
         return ["RELOADFILTERS","SHOWFILTERS"]
     }
     
     
-    
+    /*
+        Handles user commands supported by the plugin.
+        Deprecated: Should be replaced with userInputCommandInvokedOnClient
+    */
     func messageSentByUser(client: IRCClient, message: String, command: String)
     {
-        //Supposedly this is deprecated and should be replaced with userInputCommandInvokedOnClient...  If it works.
         switch (command) {
-        case "RELOADFILTERS":
-            self.loadFilterSet(client);
-        case "SHOWFILTERS":
-            self.showFilters(client);
-        default:
-            return;
+            case "RELOADFILTERS":
+                self.loadFilterSet(client);
+            case "SHOWFILTERS":
+                self.showFilters(client);
+            default:
+                return;
         }
     }
     
